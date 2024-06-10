a66c27f54b718a6b274722a5f78252c58d7c4446
Vanessa Rosa <vanessanavalayo@gmail.com>
2024-06-10 07:42:01 +0300

    Beginner food delivery page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: '<h1>Hello Universe</h1>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ src/app/my-new-component/my-new-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-new-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-new-component.component.html',
+  styleUrls: ['./my-new-component.component.css']
+})
+export class MyNewComponentComponent {
+
+}
